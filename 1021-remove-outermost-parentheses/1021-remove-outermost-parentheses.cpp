1af1fb07b0094c7edfba668d81fa302dec441370
vansh agarwal <100835025+vanshcoderdotcom@users.noreply.github.com>
2023-03-10 13:13:31 +0530

    Time: 10 ms (17.09%), Space: 6.8 MB (18.10%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        stack<char>st;
+        for(int x :s)
+        {
+            
+        if(x=='(')
+        {
+            if(st.size()>0)
+            {
+                ans.push_back(x);
+
+            }
+            st.push(x);
+        }       
+            else
+            {
+                st.pop();
+                if(!st.empty())
+                {
+                    ans.push_back(x);
+                }
+                
+
+            }
+
+        
+            
+
+        }
+        return ans;
+        
+        
+        
+    }
+};
