97ec7e3092cbb196e6254af7b93d31000f07ba20
vansh agarwal <100835025+vanshcoderdotcom@users.noreply.github.com>
2023-03-22 23:01:54 +0530

    Added solution - LeetHub

---
+++ Winner of an election - GFG/winner-of-an-election.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <cstdlib>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+  
+    //Function to return the name of candidate that received maximum votes.
+    vector<string> winner(string arr[],int n)
+    {
+        string name = "";
+        int max_vote =0;
+       unordered_map<string, int>mp;
+       for(int i=0; i<n; i++)
+       {
+           mp[arr[i]]++;
+           
+       }
+       for(auto itr=mp.begin(); itr!=mp.end(); itr++)
+       {
+           
+           string key = itr->first;
+           int value = itr->second;
+           
+           if(value>max_vote)
+           {
+               
+               max_vote =value;
+               name =key;
+           }
+           
+           else if (value==max_vote)
+           {
+               if(key<name)
+               name =key;
+           }
+           
+           
+       }
+       
+       vector<string>ans;
+       ans.push_back(name);
+       string temp =to_string(max_vote);
+       ans.push_back(temp);
+       
+       return ans;
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    for(int i=0;i<t;i++)
+    {
+        
+        
+        int n;
+        cin>>n;
+        
+        string arr[n];
+        
+        for (int i=0;i<n;i++)
+        cin>>arr[i];
+        Solution obj;
+        vector<string> result = obj.winner(arr,n);
+        
+        cout<<result[0] << " " << result[1] << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
