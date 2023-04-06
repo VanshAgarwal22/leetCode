d9888a61292af1085d64066c24fd178e1842e355
vansh agarwal <100835025+vanshcoderdotcom@users.noreply.github.com>
2023-04-06 18:38:22 +0530

    Time: 5 ms (31.60%), Space: 6 MB (64.68%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int trailingZeroes(int n)
+     {
+        int res= 0;
+        while(n)
+        {
+            n= n/5;
+            res+=n;
+            
+
+        }
+        return res;
+    }
+};
