60ab2ae0f9a283ee328a4d4e42814493de4a62f4
vansh agarwal <100835025+vanshcoderdotcom@users.noreply.github.com>
2023-04-10 09:44:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (66.07%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if(b==0)return a;
+        int sum = a^b;
+        int cr =(unsigned int)(a&b)<<1;
+        return getSum(sum ,cr);
+        
+    }
+};
