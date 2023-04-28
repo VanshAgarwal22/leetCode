eec991ede775aa48eef33c38f6a5d63cd3f3fc0f
vansh agarwal <100835025+vanshcoderdotcom@users.noreply.github.com>
2023-04-28 08:08:32 +0530

    Time: 4 ms (32.02%), Space: 5.9 MB (56.74%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -1,6 +1,7 @@
 class Solution {
-    public int sumOfMultiples(int n) {
-        int sum =0;
+public:
+    int sumOfMultiples(int n) {
+         int sum =0;
         for(int i=1; i<=n; i++)
         {
             if(i%3==0 || i%5==0 ||i%7==0)
@@ -12,5 +13,6 @@ class Solution {
         }
         return sum;
         
+        
     }
-}
+};
