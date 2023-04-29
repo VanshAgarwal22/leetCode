a1ad1dcd9a0de323df19ec87465a2c92ed511ce8
vansh agarwal <100835025+vanshcoderdotcom@users.noreply.github.com>
2023-04-29 09:04:39 +0530

    Time: 4 ms (54.07%), Space: 9.2 MB (54.32%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -1,31 +1,44 @@
 class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& nums) {
-        // unordered_map<int, int>mp;
+        unordered_map<int, int>mp;
         int n =nums.size();
         int count=0,count1 =0;
-        for(int i =0; i<n;i++)
-        {
-            for(int j =0; j<n; j++)
-            {
-                if(i!=j && nums[i]>=0 && nums[j]>=0 && nums[i]==nums[j])
-                {
-                    count++;
-                    nums[i] =-1;
-                    nums[j] =-1;
-
-                }
-
-            }
-        }
+        for(int &i :nums)
+            mp[i]++;
         
-        for(int i =0; i<n; i++)
+        for(auto &i :mp)
         {
+            count+=(i.second/2);
+            count1+=(i.second%2);
             
-            if(nums[i]>=0)
-                count1++;
+
         }
-        return {count ,count1};
+        return {count, count1};
+        
+    
+//         for(int i =0; i<n;i++)
+//         {
+//             for(int j =0; j<n; j++)
+//             {
+//                 if(i!=j && nums[i]>=0 && nums[j]>=0 && nums[i]==nums[j])
+//                 {
+//                     count++;
+//                     nums[i] =-1;
+//                     nums[j] =-1;
+
+//                 }
+
+//             }
+//         }
+        
+//         for(int i =0; i<n; i++)
+//         {
+            
+//             if(nums[i]>=0)
+//                 count1++;
+//         }
+//         return {count ,count1};
         
         
     }
