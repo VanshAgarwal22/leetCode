82af273d6d4ebb641b02cffb497b4c03faeebe6f
vansh agarwal <100835025+VanshAgarwal22@users.noreply.github.com>
2023-05-12 10:56:16 +0530

    Time: 36 ms (33.89%), Space: 23.2 MB (72.43%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    
+    bool findpath(TreeNode* root ,vector<TreeNode*>&path , TreeNode* n)
+    {
+        if(root==NULL)
+        {
+          return false;
+        }
+        path.push_back(root);
+        if(root==n)return true;
+        if(findpath(root->left ,path, n) || findpath(root->right ,path ,n))return true;
+        path.pop_back();
+        return false;
+        
+
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*>path1, path2;
+        
+        if(!findpath(root ,path1 , p) || !findpath(root, path2 ,q))return NULL;
+        
+        TreeNode* ans = NULL;
+        for(int i=0; i<path1.size()&& i<path2.size(); i++)
+        {
+            if(path1[i]==path2[i])
+                ans  =path1[i];
+
+        }
+        return ans;
+        
+    }
+};
