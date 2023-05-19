c1dbc77f583a459891a732982d8fa7b202d3ae6f
vansh agarwal <100835025+VanshAgarwal22@users.noreply.github.com>
2023-05-19 11:39:45 +0530

    Time: 128 ms (30.99%), Space: 67.2 MB (50.32%) - LeetHub

---
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numOfPairs(vector<string>& nums, string target) {
+        int n = nums.size();
+        int count =0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i!=j && nums[i]+nums[j]==target)
+                    count++;
+
+            }
+
+        }
+        return count;
+        
+    }
+};
