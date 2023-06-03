eea71cb85a1bac0cefcff6f1bb67bfcdea45839f
vansh agarwal <100835025+VanshAgarwal22@users.noreply.github.com>
2023-06-03 08:22:02 +0530

    Time: 254 ms (9.80%), Space: 30.1 MB (95.05%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+        vector<string> findRestaurant(vector<string> &a, vector<string> &b)
+        {
+            vector<string> c;
+            int min = INT_MAX;
+            for (int i = 0; i < a.size(); i++)
+            {
+                for (int j = 0; j < b.size(); j++)
+                {
+                    if (a[i] == b[j])
+                    {
+                        if (i + j < min)
+                        {
+                            min = i + j;
+                            c.clear();
+                            c.push_back(a[i]);
+                        }
+                        else if (i + j == min)
+                        {
+                            c.push_back(a[i]);
+                        }
+                    }
+                }
+            }
+            return c;
+        }
+};
